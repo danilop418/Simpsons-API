af8b430d96c3e5c6163a2c496aab0f7c48cbd0fb
LGD <daniel.lopgon.4@educa.jcyl.es>
2025-10-30 17:17:30 +0100

    Commit: 1. Refactored presentation layer architecture 2. Deleted `SimpsonsDetailActivity` and its layout 3. Renamed `SimpsonsListActivity` to `SimpsonsMainActivity` 4. Adapted `SimpsonsAdapter` to a simpler implementation

--- app/src/main/java/com/example/simpsons/features/simpsons/presentation/SimpsonsAdapter.kt
+++ app/src/main/java/com/example/simpsons/features/simpsons/presentation/SimpsonsAdapter.kt
@@ -1,53 +1,45 @@
-package com.example.simpsons.features.simpsons.presentation
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
 import com.example.simpsons.R
 import com.example.simpsons.features.simpsons.domain.Simpson
 
-class SimpsonsAdapter (private val onItemClick: (Simpson) -> Unit): RecyclerView.Adapter<SimpsonsAdapter.SimpsonsViewHolder>() {
+class SimpsonsAdapter(private val dataset: List<Simpson>) :
+    RecyclerView.Adapter<SimpsonsAdapter.ViewHolder>() {
 
-    private var simpsons: List<Simpson> = emptyList()
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val ivImage : ImageView
+        val tvName : TextView
+        val tvPhrase: TextView
 
-    fun updateSimpsons(newCharacters: List<Simpson>) {
-        simpsons = newCharacters
-        notifyDataSetChanged()
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpsonsViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_simpson, parent, false)
-        return SimpsonsViewHolder(view,onItemClick)
-    }
-
-    override fun onBindViewHolder(holder: SimpsonsViewHolder, position: Int) {
-        holder.bind(simpsons[position])
-    }
-
-    override fun getItemCount(): Int = simpsons.size
-
-    inner class SimpsonsViewHolder(itemView: View, private val onItemClick: (Simpson) -> Unit) : RecyclerView.ViewHolder(itemView) {
-        private val nameTextView: TextView = itemView.findViewById(R.id.simpson_name)
-        private val phraseTextView: TextView = itemView.findViewById(R.id.simpson_phrase)
-        private val imageView: ImageView = itemView.findViewById(R.id.simpson_image)
-
-
-        fun bind(simpsons: Simpson) {
-            nameTextView.text = simpsons.name
-            phraseTextView.text = simpsons.phrase
-
-            imageView.load(simpsons.imageUrl) {
-                crossfade(true)
-            }
-
-            itemView.setOnClickListener {
-                onItemClick(simpsons)
-            }
+        init {
+            ivImage = view.findViewById(R.id.simpson_image)
+            tvName = view.findViewById(R.id.simpson_name)
+            tvPhrase = view.findViewById(R.id.simpson_phrase)
         }
     }
-}
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_simpson, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        holder.tvName.text = dataset[position].name
+        holder.tvPhrase.text = dataset[position].phrase
+        holder.ivImage.contentDescription = dataset[position].imageUrl
+    }
+
+    override fun getItemCount() = dataset.size
+
+    }
